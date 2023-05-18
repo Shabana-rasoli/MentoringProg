e7b77f73a9f1548788b30b8a3724219009ad65e9
Shabana-rasoli <117739800+Shabana-rasoli@users.noreply.github.com>
2023-05-18 16:12:32 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# MentoringProg
---
+++ TestProject2ndtest/TestProject2ndtest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TestProject2ndtest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
